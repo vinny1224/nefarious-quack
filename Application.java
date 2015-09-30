5fe6e325e5c128578bf9ae79958df6cec4a9b1c3
vinny <vcls350@gmail.com>
2015-09-29 20:48:18 -0400

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,1 +1,35 @@
+package com.zetcode;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class Application extends JFrame {
+    
+    public Application() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setSize(250, 200);
+
+        setTitle("Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }    
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Application ex = new Application();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
